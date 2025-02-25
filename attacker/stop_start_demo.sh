94f21892b92bb6559e968c1fef8d94e78863056d
rek2 <rek2@hispagatos.org>
2025-02-25 20:36:53 +0100

    got it to not crash and curl/siege etc is running

--- attacker/stop_start_demo.sh
+++ attacker/stop_start_demo.sh
@@ -3,6 +3,7 @@ kubectl create -f attacker-deployment.yaml
 
 # Stop attacks (destructive)
 kubectl delete deployment attacker
+kubectl delete job attacker 2>/dev/null
 
 # Temporary pause (preserves logs)
 kubectl scale deployment attacker --replicas=0
