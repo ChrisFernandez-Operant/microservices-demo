00b71c8653ffe1b53565500ac17b81bfb81bfaaa
rek2 <rek2@hispagatos.org>
2025-02-25 19:23:26 +0100

    added 1 more script

---
+++ attacker/attack_scripts/3_auth_endpoint_errors.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ENDPOINTS=("signin" "signup" "auth" "authenticate")
+TOKEN="fixed_malicious_token_789"
+
+for endpoint in "${ENDPOINTS[@]}"; do
+  for i in $(seq 1 10); do
+    curl -X POST "${BASE_URL}/${endpoint}" \
+      -H "Authorization: Bearer $TOKEN" \
+      -d "username=user$i&password=wrong" &
+  done
+done
+wait
